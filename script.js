7f6e2631992d81ba0e901323b9a4394a1a141173
Sumant9761 <142734516+Sumant9761@users.noreply.github.com>
2026-05-29 14:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name: string, age: number){
+		this._name = name;
+		this._age = age;
+	}
+	get name(): string {
+		return this._name;
+	}
+	set age(newAge: number) {
+		this._age = newAge;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(): void {
+		console.log(`{this.name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(): void {
+		console.log(`{this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
